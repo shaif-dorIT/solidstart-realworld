e8c4e7c42e981cd1e0a624b7dedc74b9b34b9145
Shai Falach <shaifalach18@gmail.com>
2022-08-01 01:14:33 +0300

    fix pnpm build issue + loadcomments glitch

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,8 @@ import { defineConfig } from 'vite'
 
 import solid from 'solid-start'
 
-const viteConfig = defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    transformMode: {
-      web: [/\.[tj]sx?$/]
-    },
-    setupFiles: './scripts/setup-vitest.ts',
-    deps: {
-      inline: [/solid-js/]
-    }
-  },
-  plugins: [
-    solid({
-      ssr: true
-    })
-  ],
+export default defineConfig({
+  plugins: [solid()],
   build: {
     target: 'esnext',
     polyfillModulePreload: false
@@ -29,4 +14,3 @@ const viteConfig = defineConfig({
     conditions: ['development', 'browser']
   }
 })
-export default viteConfig
