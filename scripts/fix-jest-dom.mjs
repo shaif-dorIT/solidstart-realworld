f064b71ea2e6e8e0c417663d4757bee37788aa5d
Shai Falach <shaifalach18@gmail.com>
2022-08-01 12:16:25 +0300

    fixed script for jest-dom

--- scripts/fix-jest-dom.mjs
+++ scripts/fix-jest-dom.mjs
@@ -1,23 +1,44 @@
 import fs from 'fs'
 import path from 'path'
 
-const typesPath = path.resolve(
+const client_adapter = path.resolve(
   'node_modules',
-  '@types',
-  'testing-library__jest-dom',
-  'index.d.ts'
+  'solid-start',
+  'client-adapter.js'
 )
-const refMatcher = /[\r\n]+\/\/\/ <reference types="jest" \/>/
 
-fs.readFile(typesPath, 'utf8', (err, data) => {
+const vite_ref_matcher = /import vite from "vite";/
+fs.readFile(client_adapter, 'utf8', (err, data) => {
   if (err) throw err
-
   fs.writeFile(
-    typesPath,
-    data.replace(refMatcher, ''),
+    client_adapter,
+    data.replace(vite_ref_matcher, 'import * as vite from "vite";'),
     'utf8',
     function (error) {
       if (error) throw error
     }
   )
 })
+
+if (process.NODE_ENV !== 'production') {
+  const typesPath = path.resolve(
+    'node_modules',
+    '@types',
+    'testing-library__jest-dom',
+    'index.d.ts'
+  )
+  const refMatcher = /[\r\n]+\/\/\/ <reference types="jest" \/>/
+
+  fs.readFile(typesPath, 'utf8', (err, data) => {
+    if (err) throw err
+
+    fs.writeFile(
+      typesPath,
+      data.replace(refMatcher, ''),
+      'utf8',
+      function (error) {
+        if (error) throw error
+      }
+    )
+  })
+}
