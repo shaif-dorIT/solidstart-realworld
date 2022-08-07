cf72dc7a67e59889a8e215031f3f928c00ab82a7
Shai Falach <shaifalach18@gmail.com>
2022-08-07 15:12:09 +0300

    cleaning root compoent

--- src/root.tsx
+++ src/root.tsx
@@ -19,37 +19,21 @@ export default function Root() {
   return (
     <Html lang='en'>
       <Head>
-        <Meta charset='utf-8' />
         <Meta
+          charset='utf-8'
           name='viewport'
           content='width=device-width, initial-scale=1'
         />
         {/* <!-- Import Ionicon icons & Google Fonts our Bootstrap theme relies on --> */}
         <Link
-          href='//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'
+          href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'
           rel='stylesheet'
           type='text/css'
         />
         <Link
-          href='//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic'
+          href='https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic'
           rel='stylesheet'
           type='text/css'
-        />
-        {/* <!-- Import the custom Bootstrap 4 theme from our hosted CDN --> */}
-        <Link
-          rel='preconnect'
-          href='https://static.productionready.io'
-          crossorigin='anonymous'
-        />
-        <Link
-          rel='preconnect'
-          href='https://api.realworld.io'
-          crossorigin='anonymous'
-        />
-        <Link
-          rel='preconnect'
-          href='http://code.ionicframework.com'
-          crossorigin='anonymous'
         />
         <Link
           rel='stylesheet'
